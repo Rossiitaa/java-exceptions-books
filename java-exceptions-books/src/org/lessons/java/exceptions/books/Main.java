befdc4a1d052d8f9d5f70cd43f32340254dee122
Rossiitaa <rosscuotto1998@gmail.com>
2023-01-31 15:13:23 +0100

    initial commit

---
+++ java-exceptions-books/src/org/lessons/java/exceptions/books/Main.java
@@ -0,0 +1,9 @@
+package org.lessons.java.exceptions.books;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+	}
+}
